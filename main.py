4c1171f9840257f8688335fa5b9ed13bd188c766
Ireal <pesotskygoscha@yandex.ru>
2024-02-10 21:52:26 +0300

    faet: created reading todo objects by id and all

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
 from sqlalchemy.orm import Session
 
 from database import engine
@@ -17,3 +17,24 @@ def create_todo(todo: ToDoRequest):
     id = tododb.id
     session.close()
     return f"created todo item with id {id}"
+
+
+@app.get('/todo/{id}')
+def read_todo(id: int):
+    session = Session(bind=engine, expire_on_commit=False)
+    todo = session.query(ToDo).get(id)
+    session.close()
+
+    if not todo:
+        raise HTTPException(status_code=404, detail=f"todo item with id {id} not found")
+
+    return todo
+
+
+@app.get('/todo')
+def read_todo_list():
+    session = Session(bind=engine, expire_on_commit=False)
+    todo_list = session.query(ToDo).all()
+    session.close()
+    return todo_list
+
