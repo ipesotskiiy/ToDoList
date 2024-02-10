edf832ec11ff5e85ced93bef606ecbb3a6488bfd
Ireal <pesotskygoscha@yandex.ru>
2024-02-10 21:23:43 +0300

    feat: created POST request todo

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from sqlalchemy.orm import Session
+
+from database import engine
+from models.models import ToDo
+from schemas import ToDoRequest
 
 app = FastAPI()
+
+
+@app.post('/todo', status_code=status.HTTP_201_CREATED)
+def create_todo(todo: ToDoRequest):
+    session = Session(bind=engine, expire_on_commit=False)
+    tododb = ToDo(task=todo.task)
+    session.add(tododb)
+    session.commit()
+    id = tododb.id
+    session.close()
+    return f"created todo item with id {id}"
