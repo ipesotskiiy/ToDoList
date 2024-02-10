bb4d9e8b3a5efb12517d446699aee2377be5ebbe
Ireal <pesotskygoscha@yandex.ru>
2024-02-11 02:25:24 +0300

    ref: refactored code

--- schemas.py
+++ schemas.py
@@ -1,5 +1,13 @@
 from pydantic import BaseModel
 
 
-class ToDoRequest(BaseModel):
+class ToDoCreateSchema(BaseModel):
     task: str
+
+
+class ToDoSchema(BaseModel):
+    id: int
+    task: str
+
+    class Config:
+        orm_mode = True
