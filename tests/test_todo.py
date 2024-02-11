3e945811b65c6e3824b436d3c460bf73cb1d311c
Ireal <pesotskygoscha@yandex.ru>
2024-02-12 01:14:47 +0300

    feat: created update todo test and get todo list lest

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -12,7 +12,25 @@ def test_read_todo():
     response = client.get(f'/todo/{10}')
     assert response.status_code == 200
     assert response.json() == {
-            "id": 10,
-            "task": "Test_task"
+        "id": 10,
+        "task": "Test_task"
     }
 
+
+def test_read_todo_list():
+    response = client.get(f'/todo')
+    assert response.status_code == 200
+
+
+def test_update_todo_list():
+    response = client.put(f'/todo/{12}?task=cooking')
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 12,
+        "task": "cooking"
+    }
+
+
+def test_delete_todo():
+    response = client.delete(f'/todo/{11}')
+    assert response.status_code == 200
